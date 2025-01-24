fd1798d8e073e4bb8601630c57acb3254a2cba44
roshan-sd <143077196+roshan-sd@users.noreply.github.com>
2025-01-24 15:35:49 +0530

    merged about us

--- partials/footer.js
+++ partials/footer.js
@@ -1,3 +1,33 @@
 class Footer extends HTMLElement { constructor() { super(); }
-connectedCallback() { this.innerHTML = `<div>FOOTER</div> `; } }
+connectedCallback() { this.innerHTML = `     <div class="footer">
+        <div class="footer-top">
+          <div class="logo">
+            <img src="/assets/img/spoticar-logo.png" alt="Spoticar Logo" />
+          </div>
+          <div class="footer-nav">
+            <ul>
+              <li>About Us</li>
+              <li>Privacy Policy</li>
+              <li>Copy Right</li>
+              <li>Terms of Use</li>
+              <li>Legal, Safety and Trademark Information</li>
+            </ul>
+          </div>
+        </div>
+
+        <div class="footer-mid">
+          <div class="text">
+            <p>
+              Dealer price excludes taxes, title, registration fees, and dealer
+              fees. Pricing and offers may change at any time without
+              notification. To get full pricing details, see your dealer.
+            </p>
+          </div>
+        </div>
+
+        <div class="footer-bottom">
+          <div class="divider"></div>
+          <span>© Spoticar - 2024 | All Right Reserved</span>
+        </div>
+      </div>`; } }
 customElements.define("footer-component", Footer);
--- partials/header.js
+++ partials/header.js
@@ -1,3 +1,59 @@
 class Header extends HTMLElement { constructor() { super(); }
-connectedCallback() { this.innerHTML = `<div>HEADER</div>`; } }
+connectedCallback() { this.innerHTML = `   <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/index.html">
+            <img
+              src="/assets/img/spoticar-logo.png"
+              alt="Spoticar Logo"
+              class="d-inline-block align-text-top"
+            />
+          </a>
+
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Buy Used</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  New Car
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Jeep</a></li>
+                  <li><a class="dropdown-item" href="#">Citreon</a></li>
+                </ul>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="#">Value My Vehicle</a>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link" href="/about-us.html">About Spoticar</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>`; } }
 customElements.define("header-component", Header);
