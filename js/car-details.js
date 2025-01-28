e3d1dc37e4a2c7742158156ef6566aeac769eb69
roshan-sd <143077196+roshan-sd@users.noreply.github.com>
2025-01-28 20:17:39 +0530

    fixed car-details slider

---
+++ js/car-details.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Website Loaded!");
+  var main = new Splide("#main-slider", {
+    type: "fade",
+    rewind: true,
+    pagination: false,
+    arrows: false,
+    cover: true,
+    heightRatio: 0.5,
+  });
+
+  var thumbnails = new Splide("#thumbnail-slider", {
+    fixedWidth: 100,
+    fixedHeight: 60,
+    gap: 10,
+    rewind: true,
+    pagination: false,
+    cover: true,
+    isNavigation: true,
+    breakpoints: {
+      600: {
+        fixedWidth: 60,
+        fixedHeight: 44,
+      },
+    },
+  });
+
+  main.sync(thumbnails);
+  main.mount();
+  thumbnails.mount();
+});
